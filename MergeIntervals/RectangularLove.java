bd0e4f83342964655b400f164cb09f1c81aea422
steven-tra-n <steveetran9102@gmail.com>
2021-12-01 23:30:50 -0500

    Test commit

--- MergeIntervals/RectangularLove.java
+++ MergeIntervals/RectangularLove.java
@@ -36,4 +36,4 @@ public class RectangularLove {
 //         Rectangle lover1 = new Rectangle(1, 1, 6, 3);
 //         Rectangle lover2 = new Rectangle(8, 5, 3, 6);
 
-//         System.out.print(RectangularLove.findLove(lover1, lover2));
+//         System.out.print(RectangularLove.findLove(lover1, lover2)); 
