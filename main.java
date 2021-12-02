be23d6d0f3686acff573d5654db640c636c797a5
steven-tra-n <steventran9102@gmail.com>
2021-12-02 13:23:56 -0500

    HashMaps WordCloud

--- main.java
+++ main.java
@@ -1,4 +1,5 @@
 import MergeIntervals.*;
+import HashMaps.*;
 
 public class main{
     public static void main(String[] args){
@@ -7,6 +8,9 @@ public class main{
         Rectangle lover1 = new Rectangle(1, 1, 6, 3);
         Rectangle lover2 = new Rectangle(8, 5, 3, 6);
 
-        System.out.print(RectangularLove.findLove(lover1, lover2));
+        System.out.println(RectangularLove.findLove(lover1, lover2));
+
+        System.out.println(WordCloud.Generate("After beating the eggs, Dana read the next step:"));
+        System.out.println(WordCloud.Generate("Add milk and eggs, then add flour and sugar."));
     };
 };
