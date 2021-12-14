ba7a91a80a95183bf9f9d12a3e2b931772853ba9
steven-tra-n <steventran9102@gmail.com>
2021-12-14 00:39:20 -0500

    array_string_manipulation merge_sorted_arrays

--- array_string_manipulation/merge_sorted_arrays.py
+++ array_string_manipulation/merge_sorted_arrays.py
@@ -10,18 +10,24 @@ def merge_sorted_arrays(input, input2):
     result = []
 
     while input_index < len(input) or input2_index < len(input2):
-        if input_index < len(input):
-            if input2_index < len(input2) and input[input_index] < input2[input2_index]:
-                input_index = add_to_result(input, input_index, result)
-            else:
-                input2_index = add_to_result(input2, input2_index, result)
-        else: 
+        input_exhausted = input_index >= len(input)
+        input2_exhausted = input2_index >= len(input2)
+
+        # 2.
+        if input_exhausted:
+            input2_index = add_to_result(input2, input2_index, result)
+        elif input2_exhausted:
+            input_index = add_to_result(input, input_index, result)
+        elif input[input_index] < input2[input2_index]:
+            input_index = add_to_result(input, input_index, result)
+        else:
             input2_index = add_to_result(input2, input2_index, result)
 
     
     print(result)
     
 def add_to_result(input, index, result):
+    # 3.
     result.append(input[index])
     index += 1
 
