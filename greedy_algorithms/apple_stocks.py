93c0cfec4a198700fdead65849a6c3e8bbd76cc6
Steven Tran <steventran9102@gmail.com>
2021-12-16 15:14:00 -0500

    greedy_algorithms apple_stocks

--- greedy_algorithms/apple_stocks.py
+++ greedy_algorithms/apple_stocks.py
@@ -1,17 +1,26 @@
 def apple_stocks(stock_prices):
-    # 1. Loop through and find the lowest value so far. Store that index
-    # 2. Coninue looping, finding the highest value afterwards
-    # 3. How can we prohibit buying and seling on the same minute?
+    # 1. Initialize the first buy and sell as the highest profit so far
+    # 2. Since we can't buy and sell on the same minute, start the loop after the first index
+    # 3. Update the min and max values in the loop
 
+    if len(stock_prices) < 2:
+        raise ValueError('Getting a profit needs at least 2 prices')
+
+    # 1.
     lowest_price = stock_prices[0]
-    highest_profit = float('-inf')
+    highest_profit = stock_prices[1] - stock_prices[0]
 
+    # 2.
     for price in stock_prices[1:]:
+        # 3.
+        potential_profit = price - lowest_price
+        highest_profit = max(highest_profit, potential_profit)
+
         lowest_price = min(lowest_price, price)
-        highest_profit = max(highest_profit, price - lowest_price)
 
     print(highest_profit)
 
-stock_prices = [10, 8, 5, 1]
+stock_prices = [10, 7, 5, 11, 4]
+
 
 apple_stocks(stock_prices)
