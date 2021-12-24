10880be76894148323dd3dbd16d731d3265aaa58
Steven Tran <steventran9102@gmail.com>
2021-12-24 14:34:07 -0500

    greedy_algorithms highest_product_of_3

--- greedy_algorithms/highest_product_of_3.py
+++ greedy_algorithms/highest_product_of_3.py
@@ -6,32 +6,60 @@ def highest_product_of_3(numbers):
     # 3. Iterate through the array and try to find greater products
     # 4. Is the new product with 4 numbers greater than the max if we divide by the smallest number?
 
-    min_heap = []
-    heapq.heapify(min_heap)
-    max_product = 1
-    current_product = 0
+    # min_heap = []
+    # heapq.heapify(min_heap)
+    # max_product = 1
+    # current_product = 0
 
-    # 1.
-    for number in numbers[:3]:
-        heapq.heappush(min_heap, number)
+    # # 1.
+    # for number in numbers[:3]:
+    #     heapq.heappush(min_heap, number)
 
-        # 2.
-        max_product *= number
+    #     # 2.
+    #     max_product *= number
+
+    # for number in numbers[3:]:
+    #     min_number = heapq.heappop(min_heap)
+    #     current_product = max_product * number
+    #     current_product_after_division = current_product / min_number
+
+    #     if current_product_after_division > max_product:
+    #         max_product = current_product_after_division
+    #         heapq.heappush(min_heap, number)
+    #     else:
+    #         heapq.heappush(min_heap, min_number)
+
+    # print(max_product)
+
+    # End heap solution
+
+    # 1. Keep track of the largest and smallest (for negatives) products of 2
+    # 2. Start off with the first 2, then iterate through the rest of the array
+    # 3. Start iterating with the third element. Multiply it with the highest 2 and lowest 2
+
+    if(len(numbers) < 3):
+        raise ValueError('Need at least 3 numbers.')
+
+    # 2.
+    highest = numbers[0]
+    lowest = numbers[0]
+    highest_product_of_2 = numbers[0] * numbers[1]
+    lowest_product_of_2 = numbers[0] * numbers[1]
+
+    highest_product_of_3 = numbers[0] * numbers[1] * numbers[2]
 
     for number in numbers[3:]:
-        min_number = heapq.heappop(min_heap)
-        current_product = max_product * number
-        current_product_after_division = current_product / min_number
+        highest_product_of_3 = max(highest_product_of_3, highest_product_of_2 * number)
 
-        if current_product_after_division > max_product:
-            max_product = current_product_after_division
-            heapq.heappush(min_heap, number)
-        else:
-            heapq.heappush(min_heap, min_number)
+        highest_product_of_2 = max(highest_product_of_2, highest * number)
+        lowest_product_of_2 = min(lowest_product_of_2, lowest * number)
 
-    print(max_product)
+        highest = max(highest, number)
+        lowest = min(lowest, number)
+
+    print(highest_product_of_3)
 
 
-numbers = [-10,-10,1,3,2]
+numbers = [1, 6, 3, 4, 2]
 
 highest_product_of_3(numbers)
