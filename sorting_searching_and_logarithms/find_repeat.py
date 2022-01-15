d52b8c2c85bc072a2f8a26e95cf72ec7a0a12878
Steven Tran <steventran9102@gmail.com>
2022-01-15 15:23:29 +0700

    sorting_searching_and_logarithms find_repeat

--- sorting_searching_and_logarithms/find_repeat.py
+++ sorting_searching_and_logarithms/find_repeat.py
@@ -19,19 +19,52 @@ def find_repeat(numbers):
     # 2. Use a variable to store the current element
     # 3. Check if the element in i + 1 is equal to it
 
-    numbers.sort()
-    previous_number = numbers[0]
+    # numbers.sort()
+    # previous_number = numbers[0]
 
-    for number in numbers[1:]:
-        if number == previous_number:
-            return number
+    # for number in numbers[1:]:
+    #     if number == previous_number:
+    #         return number
 
-        previous_number = number
+    #     previous_number = number
 
-    print(numbers)
+    # print(numbers)
 
     # End solution with sort. O(n) time and O(1) space
+    # This modifies input though. Can we do this without modifying the input?
 
-numbers = [1, 4, 6, 2, 5, 3, 3]
+    # 1. Divide the list in half, seperating the list into 1 to n/2 and n/2 + 1 to n
+    # 2. Find number of possible distinct numbers for left half
+    # 3. If that number is greater than the left half, then the duplicate lives in the right half
+    # 4. Repeat
+
+    left_floor = 1
+    right_ceiling = len(numbers) - 1
+
+
+    while left_floor < right_ceiling:
+        # 1.
+        mid = left_floor + (right_ceiling - left_floor) // 2
+        left_ceiling = mid
+
+        left_half_count = 0
+
+        for number in numbers:
+            # 2.
+            if number >= left_floor and number <= left_ceiling:
+                left_half_count += 1
+
+        possible_distinct_left_numbers = left_ceiling - left_floor + 1
+
+        # 3.
+        if left_half_count > possible_distinct_left_numbers:
+                right_ceiling = mid
+        else:
+            left_floor = mid + 1
+
+    return left_floor
+
+# numbers = [1, 4, 6, 2, 5, 3, 4]
+numbers = [1, 2, 3, 4, 5, 5, 6]
 
 print(find_repeat(numbers))
