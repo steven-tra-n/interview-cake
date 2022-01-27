e637b4c054062cb312a67b9a847a2c1175b98ee4
Steven Tran <steventran9102@gmail.com>
2022-01-28 02:12:06 +0700

    dynamic_programming_and_recursion/recursive_string_permutations.py

--- dynamic_programming_and_recursion/recursive_string_permutations.py
+++ dynamic_programming_and_recursion/recursive_string_permutations.py
@@ -3,24 +3,28 @@ def get_permutations(string):
     if len(string) <= 1:
         return set([string])
 
-    all_chars_except_last = string[:-1]
+    # Break down into subproblems
+    chars_except_last = string[:-1]
     last_char = string[-1]
 
-    # Recursive call: get all possible permutations for all chars except last
-    permutations_of_all_chars_except_last = get_permutations(all_chars_except_last)
+    # Recursion
+    permutations_chars_except_last = get_permutations(chars_except_last)
 
-    # Put the last chat in all possible positions for each of the above permutations
     permutations = set()
-    for permutations_of_all_chars_except_last in permutations_of_all_chars_except_last:
-        for position in range(len(all_chars_except_last) + 1):
+
+    # Insert last char in each position
+    for permutations_chars_except_last in permutations_chars_except_last:
+        for position in range(len(chars_except_last) + 1):
             permutation = (
-                permutations_of_all_chars_except_last[:position] 
-                + last_char 
-                + permutations_of_all_chars_except_last[position:]
+                permutations_chars_except_last[:position] +
+                last_char +
+                permutations_chars_except_last[position:]
             )
+
             permutations.add(permutation)
 
     return permutations
+
 
 string = 'cats'
 
